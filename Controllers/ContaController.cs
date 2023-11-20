0a013a2162c0a8d49c6a05d580da778a58276ce2
Evaldo Junior <evaaldormj@gmail.com>
2023-11-19 21:27:21 -0300

    Feat: Controller conta'

---
+++ Controllers/ContaController.cs
@@ -0,0 +1,23 @@
+using GerenciamentoFinanceiro.Context;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciamentoFinanceiro.Controller
+{
+    [Route("Contas")]
+    [ApiController]
+    public class ContaController : ControllerBase
+    {
+        private readonly ContaContext? _context;
+
+        public ContaController(ContaContext context)
+        {
+            _context = context;
+        }
+
+        private bool ContaExists(Guid id)
+        {
+            return(_context.Contas?.Any(conta => conta.ID == id)).GetValueOrDefault();
+        }
+    }
+}
